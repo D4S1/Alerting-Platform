1feb7597578275dcde5b2126e5af6c19397079b5
Asia <joanna.bastet@gmail.com>
2026-01-30 22:43:59 +0100

    tf fix1

--- terraform/cloudrun_api.tf
+++ terraform/cloudrun_api.tf
@@ -19,10 +19,13 @@ resource "google_cloud_run_service" "api" {
 
 resource "google_secret_manager_secret" "api_url" {
   secret_id = "API_BASE_URL"
-  replication { auto {} }
+  replication {
+  auto {}
+}
 }
 
 resource "google_secret_manager_secret_version" "api_url_v" {
+  depends_on = [google_cloud_run_service.api]
   secret = google_secret_manager_secret.api_url.id
   secret_data = google_cloud_run_service.api.status[0].url
 }
