0ced80b6220008aa9a02fd9f8f1b22191f1e3114
aszymik <aniaszymik02@gmail.com>
2026-02-03 22:42:04 +0100

    rebase corrections

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,43 +13,12 @@ variable "monitoring_image" {}
 variable "notification_image" {}
 variable "ui_image" {}
 
-variable "smtp_username" {
-  type      = string
-  sensitive = true
-  default = "your-email@gmail.com"
-}
-
 variable "smtp_password" {
   type      = string
   sensitive = true
 }
 
-variable "jwt_secret" {
-  type      = string
-  sensitive = true
-}
-
-variable "smtp_host" {
-  type    = string
-  default = "smtp.gmail.com"
-}
-
-variable "smtp_port" {
-  type    = number
-  default = 587
-}
-
 variable "smtp_from" {
   type      = string
   sensitive = true
-}
-
-variable "jwt_exp_minutes" {
-  type    = number
-  default = 15
-}
-
-variable "escalation_delay_seconds" {
-  type    = number
-  default = 300
 }
